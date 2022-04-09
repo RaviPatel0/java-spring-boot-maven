65182df1907e5a85ae00c614edf7c38f200bd44d
Ravi Nandasana <ravinandasana1998@gmail.com>
2022-04-10 00:36:57 +0530

    Update comment

--- startup.sh
+++ startup.sh
@@ -1,15 +1,20 @@
 running_containers=`docker ps | grep maven-project | wc -l`
 local_images=`docker images | grep java_app | wc -l`
-echo $running_containers
-echo $local_images
+echo "------------------------>Number of running containers: $running_containers"
+echo "------------------------>Number of local images: $local_images"
 if [ $running_containers -gt 0 ]
 then
+    echo "------------------------>found containers running, removing them."
     docker-compose down
 fi
 if [ $local_images -gt 0 ]
 then
+    echo "------------------------>found local images, removing them"
     docker rmi -f java_app:1.1
 fi
+echo "------------------------>building image."
 docker build /home/ubuntu/java-maven-app -t java_app:1.1
-docker-compose up -d -f /home/ubuntu/java-maven-app/docker-compose.yaml
+echo "------------------------>running containers."
+docker-compose up -f /home/ubuntu/java-maven-app/docker-compose.yaml -d
+echo "------------------------>Current running containers"
 docker ps
